96c64edc495dcd69464b6984b93e1cd37303cfbc
Sonu Kumar <kumarsonuk430@gmail.com>
2024-01-05 22:47:24 +0530

    DOM project-1 done

---
+++ DOM_Project-1/index.js
@@ -0,0 +1,10 @@
+const boxEL = document.querySelector(".box");
+
+const headingEl = document.querySelector(".heading");
+
+boxEL.style.backgroundColor = "yellow";
+boxEL.style.color = "red";
+boxEL.style.border = "solid 5px green";
+
+headingEl.style.padding = "20px";
+headingEl.style.backgroundColor = "#145848";
