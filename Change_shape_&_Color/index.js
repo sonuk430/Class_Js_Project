b1ccf80722cc386ab51e10a15568a1af1d7f83f0
Sonu Kumar <kumarsonuk430@gmail.com>
2024-01-09 14:10:03 +0530

    creating color and shap change

---
+++ Change_shape_&_Color/index.js
@@ -0,0 +1,50 @@
+let circleEL = document.querySelector(".circle");
+
+let colorBtnEl = document.querySelector("#colorBtn");
+
+let shapeBtnEl = document.querySelector("#shapeBtn");
+
+let colorArr = [
+  "red",
+  "yellow",
+  "blue",
+  "aqua",
+  "orangered",
+  "brown",
+  "violet",
+];
+
+let shapArr = [
+  "triangle-up",
+  "triangle-down",
+  "triangle-left",
+  "triangle-right",
+  "pacman",
+  "moon",
+];
+
+let colorIndex = 0;
+let shapIndex = 0;
+colorBtnEl.addEventListener("click", () => {
+  let color = colorArr[colorIndex];
+
+  circleEL.style.backgroundColor = color;
+  colorIndex++;
+  console.log(color);
+  if (colorIndex == colorArr.length) {
+    colorIndex = 0;
+  }
+});
+
+shapeBtnEl.addEventListener("click",()=>{
+
+    shap = shapArr[shapIndex];
+    circleEL.classList.add("shap");
+    shapIndex++;
+    console.log(shap);
+    if(shapIndex == shapArr.length){
+        shapIndex = 0;
+    }
+
+
+})
