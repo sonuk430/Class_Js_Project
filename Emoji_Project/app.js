5779e832c8b7704bd5f2c2ebc04d082778aa14d7
Sonu Kumar <kumarsonuk430@gmail.com>
2024-01-16 22:16:19 +0530

    emoJiList display on screen

---
+++ Emoji_Project/app.js
@@ -0,0 +1,30 @@
+window.onload = () => display;
+
+let search_field = document.querySelector("#search_field");
+
+search_field.addEventListener("keyup", (e) => {
+  let serachValue = search_field.value;
+
+  console.log(e);
+});
+
+function display() {
+  let search_result_container = document.querySelector(
+    "#search_result_container"
+  );
+  //   console.log(search_result_container);
+
+  emojiList.forEach((el) => {
+    let newRow = document.createElement("tr");
+
+    newRow.innerHTML = `<tr>
+    <td>${el.emoji}</td>
+    <td>${el.description}</td>
+    <td>${el.aliases}</td>
+  </tr>`;
+
+    search_result_container.appendChild(newRow);
+  });
+}
+
+display();
